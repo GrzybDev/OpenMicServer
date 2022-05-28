59bd0208c59ef22b91b94bc05a40739d8dcd94f9
Marek Grzyb <grzybdev@gmail.com>
2022-05-28 20:55:09 +0200

    Added hello handler

--- src/net/packets/client/packetclientsystem.cpp
+++ src/net/packets/client/packetclientsystem.cpp
@@ -1,7 +1,55 @@
 #include "packetclientsystem.h"
 
+#include <QCoreApplication>
+#include "../../handler.h"
+#include <QSysInfo>
+
 PacketClientSystem::PacketClientSystem(QObject *parent)
     : QObject{parent}
 {
 
+}
+
+QString PacketClientSystem::Handle(CLIENT_MESSAGE type, QJsonObject data)
+{
+    switch (type) {
+        case CLIENT_SYSTEM_HELLO:
+            return handleHello(data);
+        default:
+            return "";
+    }
+}
+
+QString PacketClientSystem::handleHello(QJsonObject data)
+{
+    QString clientApp = data.value("clientApp").toString();
+    QString clientVersion = data.value("clientVersion").toString();
+
+    QJsonObject response;
+
+    qDebug() << "Connected client:" << clientApp << "(Version:" << clientVersion << ")";
+
+    bool clientValid = false;
+
+    if (clientApp == "pl.grzybdev.openmic.client")
+    {
+        if (clientVersion == QCoreApplication::applicationVersion())
+            clientValid = true;
+    }
+    else if (clientApp != "pl.grzybdev.openmic.client")
+        clientValid = true;
+
+    if (clientValid) {
+        response["serverApp"] = QCoreApplication::applicationName();
+        response["serverVersion"] = QCoreApplication::applicationVersion();
+        response["serverOS"] = QSysInfo::kernelType();
+        response["serverName"] = QSysInfo::machineHostName();
+    }
+    else
+    {
+        response["error"] = "ERR_VERSION_MISMATCH";
+        response["message"] = tr("Version mismatch between client and server!\nIf you're using official apps, please make sure that both client and server have equal version numbers.\n\nCurrent values:\nClient version: %1\nServer version: %2").arg(clientVersion, QCoreApplication::applicationVersion());
+    }
+
+    return Handler::GetResponse(SERVER_SYSTEM_HELLO, response);
 }
--- src/net/packets/client/packetclientsystem.h
+++ src/net/packets/client/packetclientsystem.h
@@ -2,15 +2,20 @@
 #define PACKETCLIENTSYSTEM_H
 
 #include <QObject>
+#include "../packet.h"
 
-class PacketClientSystem : public QObject
+class PacketClientSystem : public QObject, public Packet
 {
     Q_OBJECT
+    Q_INTERFACES(Packet)
+
 public:
     explicit PacketClientSystem(QObject *parent = nullptr);
 
-signals:
+    QString Handle(CLIENT_MESSAGE type, QJsonObject data);
 
+private:
+    QString handleHello(QJsonObject data);
 };
 
 #endif // PACKETCLIENTSYSTEM_H
