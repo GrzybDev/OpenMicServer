59108e53ef8a5e7746d3a75a375918619a914886
Marek Grzyb <grzybdev@gmail.com>
2022-05-28 14:10:00 +0200

    Reformatted main.cpp and load Qt locale with App locale

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,42 @@
-#include "src/ui/WindowMain/mainwindow.h"
+#include "ui/WindowMain/mainwindow.h"
 
 #include <QApplication>
+#include <QLibraryInfo>
 #include <QLocale>
 #include <QTranslator>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
 
-    QTranslator translator;
+    QTranslator appTranslator;
+    QTranslator qtTranslator;
+
     const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "OpenMicServer_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
+
+    for (const QString &localeCode : uiLanguages) {
+        const QLocale locale = QLocale(localeCode);
+
+        const QString localeName = locale.name();
+        const QString localeShortName = localeName.split('_').first();
+        const QString languageName = locale.languageToString(locale.language());
+
+        const QString appLocalePath = ":/i18n/OpenMicServer_" + localeName;
+        const QString qtLocalePath = "qt_" + localeShortName;
+
+        if (appTranslator.load(appLocalePath) && qtTranslator.load(qtLocalePath, QLibraryInfo::path(QLibraryInfo::TranslationsPath))) {
+            app.installTranslator(&appTranslator);
+            app.installTranslator(&qtTranslator);
+
+            qDebug() << "Successfully set application language to" << languageName;
             break;
         }
+
+        qDebug() << "Failed to set application language to" << languageName << "(" << localeName << "), either application or Qt doesn't support your language!";
     }
-    MainWindow w;
-    w.show();
-    return a.exec();
+
+    MainWindow win;
+    win.show();
+
+    return app.exec();
 }
