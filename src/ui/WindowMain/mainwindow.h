2c1652942ea030e55ae2eb7ef7fc4ba076a07a4c
Marek Grzyb <grzybdev@gmail.com>
2022-05-28 15:54:11 +0200

    Initialize OpenMic object in MainWindow

--- src/ui/WindowMain/mainwindow.h
+++ src/ui/WindowMain/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "../../openmic.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    OpenMic* openmic;
 };
 #endif // MAINWINDOW_H
