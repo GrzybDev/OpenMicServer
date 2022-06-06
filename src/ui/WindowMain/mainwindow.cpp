dd925b0471911de90a280da36b2b1977686b685f
Marek Grzyb <grzybdev@gmail.com>
2022-06-06 14:04:38 +0200

    Finished MainWindow design

--- src/ui/WindowMain/mainwindow.cpp
+++ src/ui/WindowMain/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QMovie>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    initStatus();
 
     openmic = new OpenMic(this);
 }
@@ -15,3 +19,19 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::initStatus()
+{
+    QMovie *spinner = new QMovie(":/Spinner-1s-48px.gif");
+    spinner->start();
+
+    QList<QLabel*> lblList;
+    lblList.append(ui->btStatus);
+    lblList.append(ui->usbStatus);
+    lblList.append(ui->wifiStatus);
+
+    foreach (QLabel* lbl, lblList)
+    {
+        lbl->setAttribute(Qt::WA_NoSystemBackground);
+        lbl->setMovie(spinner);
+    }
+}
--- src/ui/WindowMain/mainwindow.h
+++ src/ui/WindowMain/mainwindow.h
@@ -19,5 +19,7 @@ public:
 private:
     Ui::MainWindow *ui;
     OpenMic* openmic;
+
+    void initStatus();
 };
 #endif // MAINWINDOW_H
