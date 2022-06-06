d7001fe845f351fa3e7e274ace415da2a3139255
Marek Grzyb <grzybdev@gmail.com>
2022-06-06 14:04:53 +0200

    Updated Polish locale file

--- src/lang/OpenMicServer_pl_PL.ts
+++ src/lang/OpenMicServer_pl_PL.ts
@@ -1,3 +1,206 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pl_PL"></TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>DeviceAuthDialog</name>
+    <message>
+        <location filename="../ui/DialogDeviceAuth/deviceauthdialog.ui" line="26"/>
+        <source>OpenMic Server | Pairing new device</source>
+        <translation>Serwer OpenMic | Parowanie nowego urządzenia</translation>
+    </message>
+    <message>
+        <location filename="../ui/DialogDeviceAuth/deviceauthdialog.ui" line="63"/>
+        <source>Pair new device 📱</source>
+        <translation>Sparuj nowe urządzenie 📱</translation>
+    </message>
+    <message>
+        <location filename="../ui/DialogDeviceAuth/deviceauthdialog.ui" line="73"/>
+        <source>Please enter this code on your mobile device:</source>
+        <translation>Proszę podać ten kod na twoim urządzeniu mobilnym:</translation>
+    </message>
+    <message>
+        <location filename="../ui/DialogDeviceAuth/deviceauthdialog.cpp" line="57"/>
+        <source>Code you provided is invalid for this server.</source>
+        <translation>Wprowadzony kod nie jest prawidłowy dla tego serwera.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="26"/>
+        <source>OpenMic Server</source>
+        <translation>Serwer OpenMic</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="43"/>
+        <source>Ready for action! 😎</source>
+        <translation>Gotowy do akcji! 😎</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="61"/>
+        <source>Server is now ready for your mobile device! 📱</source>
+        <translation>Serwer jest już gotów na twoje urządzenie mobilne! 📱</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="74"/>
+        <source>If you haven&apos;t already, download &quot;OpenMic Client&quot; to your phone, and then:
+
+- If you want to use USB, just connect your phone to PC via cable
+- If you want to use Wi-Fi, connect your phone to the same network as your PC and then follow instructions shown on your mobile device.
+- If you want to use Bluetooth, pair your phone with your PC and then follow instructions shown on your mobile device.</source>
+        <translation>Jeżeli wcześniej tego nie zrobiłeś, pobierz &quot;Klienta OpenMic&quot; na twój telefon, i później:
+
+- Jeżeli chcesz użyć USB, po prostu połącz twój telefon do komputera za pomocą kabla
+- Jeżeli chcesz użyć Wi-Fi, połącz swój telefon do sieci w której jest twój komputer i wykonaj instrukcje pokazane na twoim telefonie.
+- Jeżeli chcesz użyć Bluetooth, sparuj telefon z twoim komputerem i wykonuj instrukcje pokazane na twoim telefonie.</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="99"/>
+        <source>Wired (USB)</source>
+        <translation>Przewodowo (USB)</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="141"/>
+        <source>Wireless (Wi-Fi)</source>
+        <translation>Bezprzewodowo (Wi-Fi)</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="190"/>
+        <source>Wireless (Bluetooth)</source>
+        <translation>Bezprzewodowo (Bluetooth)</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="217"/>
+        <source>You can connect your phone to this PC by:</source>
+        <translation>Możesz połączyć telefon z tym komputerem za pomocą:</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="237"/>
+        <source>Connection</source>
+        <translation>Połączenie</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="245"/>
+        <source>Settings</source>
+        <translation>Ustawienia</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="254"/>
+        <source>Help</source>
+        <translation>Pomoc</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="263"/>
+        <source>Donate</source>
+        <translation>Dotacja</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="279"/>
+        <source>Disconnect</source>
+        <translation>Rozłącz</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="288"/>
+        <source>Exit</source>
+        <translation>Wyjdź</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="297"/>
+        <source>Audio</source>
+        <translation>Dźwięk</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="306"/>
+        <source>Network</source>
+        <translation>Sieć</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="315"/>
+        <source>System</source>
+        <translation>System</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="324"/>
+        <source>FAQ</source>
+        <translation>Często zadawane pytania</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="333"/>
+        <source>Tutorial</source>
+        <translation>Samouczek</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="342"/>
+        <source>About</source>
+        <translation>O programie</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="347"/>
+        <source>...to OpenMic author</source>
+        <translation>...dla autora OpenMic</translation>
+    </message>
+    <message>
+        <location filename="../ui/WindowMain/mainwindow.ui" line="356"/>
+        <source>Devices</source>
+        <translation>Urządzenia</translation>
+    </message>
+</context>
+<context>
+    <name>PacketSystem</name>
+    <message>
+        <location filename="../net/packets/system.cpp" line="76"/>
+        <source>Version mismatch between client and server!
+
+If you&apos;re using official apps, please make sure that both client and server have equal version numbers.
+
+Client version: %1
+Server version: %2</source>
+        <translation>Wersja klienta oraz serwera nie jest taka sama!
+
+Jeżeli używasz oficjalnych aplikacji, upewnij się że zarówno klient i serwer są w identycznej wersji.
+
+Wersja klienta: %1
+Wersja serwera: %2</translation>
+    </message>
+</context>
+<context>
+    <name>Server</name>
+    <message>
+        <location filename="../net/server.cpp" line="24"/>
+        <source>Connection denied. Another client is already connected!</source>
+        <translation>Połączenie odrzucone. Inny klient jest już połączony!</translation>
+    </message>
+    <message>
+        <location filename="../net/server.cpp" line="56"/>
+        <location filename="../net/server.cpp" line="63"/>
+        <source>Invalid text data, disconnecting...</source>
+        <translation>Nieprawidłowe dane, rozłączanie...</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../settings.cpp" line="95"/>
+        <source>OpenMic Server - Configuration validation failed</source>
+        <translation>Serwer OpenMic - Błąd weryfikacji konfiguracji</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="96"/>
+        <source>Invalid network communication port specified!</source>
+        <translation>Ustawiono nieprawidłowy port komunikacji!</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="97"/>
+        <source>Offline configuration change for communication port in network section were out of acceptable range. Network communication port have been reset to default value.</source>
+        <translation>Zmiana zrobiona poza aplikacją obejmowała port komunikacyjny w sekcji Sieć, port ten był poza akceptowalnymi wartościami. Port komunikacji został zresetowany do domyślnej wartości.</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="98"/>
+        <source>Network communication port have been set to: %1
+
+Previous (invalid) value: %2 (out of range, valid range: %3-%4)</source>
+        <translation>Port komunikacji został ustawiony na: %1
+Poprzednia (nieprawidłowa) wartość: %2 (poza zakresem, poprawny zakres to: %3-%4)</translation>
+    </message>
+</context>
+</TS>
