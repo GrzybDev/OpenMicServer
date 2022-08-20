b4e35cddba6f0c31d5acb45ca94ad2970e137128
Marek Grzyb <grzybdev@gmail.com>
2022-08-20 16:05:16 +0200

    Removed singleton related stuff from PacketAuth

--- src/net/packets/auth.hpp
+++ src/net/packets/auth.hpp
@@ -11,16 +11,6 @@ class PacketAuth : public Packet
     Q_INTERFACES(Packet)
 public:
     explicit PacketAuth(QObject *parent = nullptr);
-    PacketAuth(const PacketAuth&): PacketAuth(nullptr) {}
-
-    static PacketAuth & getInstance() {
-        static PacketAuth * _instance = 0;
-
-        if ( _instance == 0 )
-            _instance = new PacketAuth();
-
-        return *_instance;
-    }
 
     QString Handle(MESSAGE type, QJsonObject data) override;
 
